71162597807e178107865f7f97a086e96d6fe09a
zhengjiaxin5 <1814409761@qq.com>
2019-11-12 17:10:28 +0800

    框架

---
+++ src/com/webtest/demo/demo.java
@@ -0,0 +1,15 @@
+package com.webtest.demo;
+
+import static org.testng.Assert.assertTrue;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+//import com.edu.core.BaseTest;
+
+public class demo {
+	//extends BaseTest
+	@Test()
+	public void demo() {
+		System.out.println("hello");
+	}
+}
+
