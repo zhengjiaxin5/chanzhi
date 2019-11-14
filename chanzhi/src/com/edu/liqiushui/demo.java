3906a1b85bf4f2dfb643835a7333690ab7505c5f
zhengjiaxin5 <1814409761@qq.com>
2019-11-14 10:26:07 +0800

    框架

---
+++ chanzhi/src/com/edu/liqiushui/demo.java
@@ -0,0 +1,14 @@
+package com.edu.liqiushui;
+
+import static org.testng.Assert.assertTrue;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class demo {
+	@Test()
+	public void demo() {
+		System.out.println("hello");
+	}
+}
+
