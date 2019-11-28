5bd84fb3c650fb9244c9b859594f823af4ad110e
wfybxq <33412136+wfybxq@users.noreply.github.com>
2019-11-28 20:32:24 +0800

    Create Admin_Login.java

---
+++ chanzhi/src/com/edu/baoxuanqi/Admin_Login.java
@@ -0,0 +1,21 @@
+package com.webtest.demo;
+
+import static org.testng.Assert.assertTrue;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.webtest.core.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.webtest.core.BaseTest;
+public class Admin_Login  extends BaseTest{
+	
+	@Test
+	public void testLogin() {
+		//����ҳ��
+		webtest.open("http://127.0.0.1/chanzhieps/www/admin.php");
+		webtest.type("name=account", "admin");
+		webtest.type("name=password", "admin");
+		webtest.click("xpath=//*[@id=\"submit\"]");
+	}
+
+}
