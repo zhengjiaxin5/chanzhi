52fb1b1ba12aa02df9e4c11dbb1a17acabed8c9d
李秋水 <496629735@qq.com>
2019-12-03 14:40:00 +0800

    shuii

--- chanzhi/src/com/edu/liqiushui/Block.java
+++ chanzhi/src/com/edu/liqiushui/Block.java
@@ -7,7 +7,6 @@ import com.webtest.demo.Admin_Login;
 public class Block extends Admin_Login{
 	@Test(description="自定义区块添加一个区块")
 	public void test9() {
-		testLogin();
 		webtest.click("link='设计'");
 		webtest.click("link='区块'");
 		webtest.click("/html/body/div/div[2]/div[1]/div/div[1]/div/a/i");//添加
@@ -17,7 +16,6 @@ public class Block extends Admin_Login{
 	}
 	@Test(description="删除一个区块")
 	public void test10() {
-		testLogin();
 		webtest.click("link='设计'");
 		webtest.click("link='区块'");
 		webtest.click("/html/body/div[1]/div[2]/div[1]/div/div[2]/span[1]/a[2]/i");//删除区块
--- chanzhi/src/com/edu/liqiushui/Theme1.java
+++ chanzhi/src/com/edu/liqiushui/Theme1.java
@@ -9,17 +9,46 @@ import org.testng.annotations.Test;
 import com.webtest.core.BaseTest;
 
 public class Theme1 extends Admin_Login1{
-	@Test
+	@Test(description="切换主题1")
 	public void test2() {
 		webtest.click("link=设计");
 		webtest.click("xpath=//*[@id=\"internalSection\"]/div[1]/div/a/img");
 		assertTrue(webtest.isTextPresent("设置成功"));
 	}
-	@Test
+	@Test(description="切换主题2")
 	public void test3() {
 		webtest.click("link=设计");
 		webtest.click("xpath=//*[@id=\"internalSection\"]/div[2]/div/a/img");
 		assertTrue(webtest.isTextPresent("设置成功"));
+		
+	}
+	@Test(description="切换主题3")
+	public void test4() {
+		webtest.click("link=设计");
+		webtest.click("xpath=//*[@id=\"internalSection\"]/div[3]/div/a/img");
+		assertTrue(webtest.isTextPresent("设置成功"));
+		
+	}
+	@Test(description="切换主题4")
+	public void test5() {
+		webtest.click("link=设计");
+		webtest.click("xpath=//*[@id=\"internalSection\"]/div[4]/div/a/img");
+		assertTrue(webtest.isTextPresent("设置成功"));
+		
+	}
+	@Test(description="切换空白主题")
+	public void test6() {
+		webtest.click("link=设计");
+		webtest.click("xpath=//*[@id=\"internalSection\"]/div[5]/div/a/img");
+		assertTrue(webtest.isTextPresent("设置成功"));
+		
+	}
+	@Test(description="自定义主题页面")
+	public void test7() {
+		webtest.click("link=设计");
+		webtest.click("xpath=//*[@id=\"internalSection\"]/div[5]/div/a/img");
+		webtest.click("xpath=//*[@id=\"internalSection\"]/div[5]/div/div[2]/a");
+		assertTrue(webtest.isTextPresent("设置成功"));
+		
 	}
 }
-//
