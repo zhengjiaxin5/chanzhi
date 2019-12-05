2ad7ba7f3b6691b56223f438afcc8b2537401581
wangxiaodan521 <1739979001@qq.com>
2019-12-05 08:31:25 +0800

    dan

--- chanzhi/src/com/edu/wangxiaodan/content.java
+++ chanzhi/src/com/edu/wangxiaodan/content.java
@@ -4,6 +4,7 @@ import static org.testng.Assert.assertTrue;
 
 import java.io.IOException;
 
+import org.openqa.selenium.injector.UnableToInstaniateInstanceException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -17,7 +18,7 @@ public class content extends Admin_Login{
 	@DataProvider
 	public Object[][] name() throws IOException {
 		ExcelDataProvider excelDataProvider=new ExcelDataProvider();
-		Object[][] s=excelDataProvider.getTestDataByExcel("F://nnn.xls", "sheet2");
+		Object[][] s=excelDataProvider.getTestDataByExcel("F://nnn.xlsx", "sheet2");
 		return s;
 	}
 	
@@ -29,13 +30,13 @@ public class content extends Admin_Login{
 	 */
 	public void test1(String name,String name1) throws IOException{
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=文章");
-		webtest.click("link=维护类目");
+		webtest.click1("link=内容");
+		webtest.click1("link=文章");
+		webtest.click1("link=维护类目");
 		webtest.type("id=children[]", name);
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent(name1));
 		
 		
@@ -43,19 +44,19 @@ public class content extends Admin_Login{
 	@Test(description="在目录一下添加一篇文章,标题不能重复",priority=2)
 	public void test2() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=文章");
+		webtest.click1("link=内容");
+		webtest.click1("link=文章");
 		boolean s=webtest.isTextPresent("这是一个title");
-		webtest.click("link=目录一");
-		webtest.click("link=发布文章");
+		webtest.click1("link=目录一");
+		webtest.click1("link=发布文章");
 		webtest.type("id=title", "这是一个title");
 		webtest.enterFrame(0);
-		webtest.click("tag=body");
+		webtest.click1("tag=body");
 		webtest.type("tag=body", "这是第一篇文章的内容");
 		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		if(s==true) {
 			assert(webtest.isTextPresent("这是一个title"));
 		}
@@ -69,237 +70,247 @@ public class content extends Admin_Login{
 	@DataProvider
 	public Object[][] name1() throws IOException {
 		ExcelDataProvider excelDataProvider=new ExcelDataProvider();
-		Object[][] s=excelDataProvider.getTestDataByExcel("F://nnn.xls", "sheet3");
+		Object[][] s=excelDataProvider.getTestDataByExcel("F://nnn.xlsx", "sheet3");
 		return s;
 	}
 	
 	@Test(description="在目录一下添加一篇文章",dataProvider="name1",priority=3)
 	public void test3(String title,String content,String asserts) throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=文章");
-		webtest.click("link=目录一");
-		webtest.click("link=发布文章");
+		webtest.click1("link=内容");
+		webtest.click1("link=文章");
+		webtest.click1("link=目录一");
+		webtest.click1("link=发布文章");
 		webtest.type("id=title", title);
 		webtest.enterFrame(0);
-		webtest.click("tag=body");
+		webtest.click1("tag=body");
 		webtest.type("tag=body", content);
 		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent(asserts));
+	}
+	@Test(description="删除目录二",priority=4)
+	public void test28() throws IOException {
+		testLogin();
+		webtest.click1("link=内容");
+		webtest.click1("link=文章");
+		webtest.click1("link=维护类目");
+		webtest.click1("xpath=//li[@data-id='6']/a[@class='deleter']");
+		webtest.click1("xpath=/html/body/div[2]/div/div/div[2]/button[1]");
+		assertTrue(webtest.isTextPresent("目录二"));
 	}
 	@Test(description="在目录一下添加一篇title为空的文章",priority=4)
 	public void test4() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=文章");
-		webtest.click("link=目录一");
-		webtest.click("link=发布文章");
+		webtest.click1("link=内容");
+		webtest.click1("link=文章");
+		webtest.click1("link=目录一");
+		webtest.click1("link=发布文章");
 //		webtest.type("id=title", );
 		webtest.enterFrame(0);
-		webtest.click("tag=body");
+		webtest.click1("tag=body");
 		webtest.type("tag=body", "title为空");
 		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("不能为空"));
 		
 	}
 	@Test(description="在目录一下添加一篇内容为空的文章",priority=5)
 	public void test5() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=文章");
-		webtest.click("link=目录一");
-		webtest.click("link=发布文章");
+		webtest.click1("link=内容");
+		webtest.click1("link=文章");
+		webtest.click1("link=目录一");
+		webtest.click1("link=发布文章");
 		webtest.type("id=title","内容为空" );
 //		webtest.enterFrame(0);
-//		webtest.click("tag=body");
+//		webtest.click1("tag=body");
 //		webtest.type("tag=body", "title为空");
 //		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("不能为空"));
 		
 	}
 	@Test(description="对文章输入内容搜索",priority=6)
 	public void test6() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=文章");
+		webtest.click1("link=内容");
+		webtest.click1("link=文章");
 		webtest.type("id=searchWord", "title");
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("title"));
 	}
 	@Test(description="直接点击搜索",priority=7)
 	public void test7() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=文章");
-		webtest.click("id=submit");
+		webtest.click1("link=内容");
+		webtest.click1("link=文章");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("请输入搜索内容"));
 	}
 	@Test(description="添加单页",priority=8)
 	public void test8() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=单页");
-		webtest.click("link=添加单页");
+		webtest.click1("link=内容");
+		webtest.click1("link=单页");
+		webtest.click1("link=添加单页");
 		webtest.type("id=title", "第一个单页title");
 		webtest.enterFrame(0);
-		webtest.click("tag=body");
+		webtest.click1("tag=body");
 		webtest.type("tag=body", "第一个单页内容");
 		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("第一个单页title"));
 	}
 	@Test(description="添加title为空的单页",priority=9)
 	public void test9() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=单页");
-		webtest.click("link=添加单页");
+		webtest.click1("link=内容");
+		webtest.click1("link=单页");
+		webtest.click1("link=添加单页");
 //		webtest.type("id=title", "第一个单页title");
 		webtest.enterFrame(0);
-		webtest.click("tag=body");
+		webtest.click1("tag=body");
 		webtest.type("tag=body", "第一个单页内容");
 		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("不能为空"));
 	}
 	@Test(description="添加内容为空的单页",priority=10)
 	public void test10() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=单页");
-		webtest.click("link=添加单页");
+		webtest.click1("link=内容");
+		webtest.click1("link=单页");
+		webtest.click1("link=添加单页");
 		webtest.type("id=title", "第二个单页title");
 //		webtest.enterFrame(0);
-//		webtest.click("tag=body");
+//		webtest.click1("tag=body");
 //		webtest.type("tag=body", "第一个单页内容");
 //		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("第一个单页title"));
 	}
 	@Test(description="对单页输入内容搜索",priority=11)
 	public void test11() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=单页");
+		webtest.click1("link=内容");
+		webtest.click1("link=单页");
 		webtest.type("id=searchWord", "title");
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("title"));
 	}
 	@Test(description="单页直接点击搜索",priority=12)
 	public void test12() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=单页");
-		webtest.click("xpath=//input[@id='submit']");
+		webtest.click1("link=内容");
+		webtest.click1("link=单页");
+		webtest.click1("xpath=//input[@id='submit']");
 		assertTrue(webtest.isTextPresent("请输入搜索内容"));
 	}
 	@Test(description="博客下添加类目",priority=13)
 	public void test13() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=博客");
-		webtest.click("link=维护类目");
+		webtest.click1("link=内容");
+		webtest.click1("link=博客");
+		webtest.click1("link=维护类目");
 		webtest.type("id=children[]", "第一类目");
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("第一类目"));
 	}
 	@Test(description="第一类目下添加博客",priority=14)
 	public void test14() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=博客");
-		webtest.click("link=第一类目");
-		webtest.click("link=发布博客");
+		webtest.click1("link=内容");
+		webtest.click1("link=博客");
+		webtest.click1("link=第一类目");
+		webtest.click1("link=发布博客");
 		webtest.type("id=title", "博客title");
 		webtest.enterFrame(0);
-		webtest.click("tag=body");
+		webtest.click1("tag=body");
 		webtest.type("tag=body", "第一个单页内容");
 		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("博客title"));
 		
 	}
 	@Test(description="第一类目下添加title为空的博客",priority=15)
 	public void test15() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=博客");
-		webtest.click("link=第一类目");
-		webtest.click("link=发布博客");
+		webtest.click1("link=内容");
+		webtest.click1("link=博客");
+		webtest.click1("link=第一类目");
+		webtest.click1("link=发布博客");
 //		webtest.type("id=title", "博客title");
 		webtest.enterFrame(0);
-		webtest.click("tag=body");
+		webtest.click1("tag=body");
 		webtest.type("tag=body", "第一个单页内容");
 		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("不能为空"));
 		
 	}
 	@Test(description="第一类目下添加内容为空博客",priority=16)
 	public void test16() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=博客");
-		webtest.click("link=第一类目");
-		webtest.click("link=发布博客");
+		webtest.click1("link=内容");
+		webtest.click1("link=博客");
+		webtest.click1("link=第一类目");
+		webtest.click1("link=发布博客");
 		webtest.type("id=title", "博客title");
 //		webtest.enterFrame(0);
-//		webtest.click("tag=body");
+//		webtest.click1("tag=body");
 //		webtest.type("tag=body", "第一个单页内容");
 //		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("不能为空"));
 		
 	}
 	@Test(description="对博客输入内容搜索",priority=17)
 	public void test17() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=博客");
+		webtest.click1("link=内容");
+		webtest.click1("link=博客");
 		webtest.type("id=searchWord", "博客title");
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("title"));
 	}
 	@Test(description="博客直接点击搜索",priority=18)
 	public void test18() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=博客");
-		webtest.click("xpath=//input[@id='submit']");
+		webtest.click1("link=内容");
+		webtest.click1("link=博客");
+		webtest.click1("xpath=//input[@id='submit']");
 		assertTrue(webtest.isTextPresent("请输入搜索内容"));
 	}
 	@Test(description="添加手册下的类目",priority=19)
 	public void test19() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=手册");
-		webtest.click("link=添加");
+		webtest.click1("link=内容");
+		webtest.click1("link=手册");
+		webtest.click1("link=添加");
 		webtest.type("id=title", "用户手册");
 		webtest.type("id=alias", "这是用户手册");
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("xpath=//*[@id='submit']");
+		webtest.click1("xpath=//*[@id='submit']");
 		assertTrue(webtest.isTextPresent("用户手册"));
 	}
 	@DataProvider
@@ -311,62 +322,62 @@ public class content extends Admin_Login{
 	@Test(description="用户手册下面添加章节",priority=20,dataProvider="name3")
 	public void test20(String title1,String title2) throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=手册");
-		webtest.click("link=用户手册");
-		webtest.click("link=章节");
+		webtest.click1("link=内容");
+		webtest.click1("link=手册");
+		webtest.click1("link=用户手册");
+		webtest.click1("link=章节");
 		webtest.type("id=title[]", title1);
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("xpath=//*[@id='submit']");
+		webtest.click1("xpath=//*[@id='submit']");
 		assertTrue(webtest.isTextPresent(title2));
 	}
 	@Test(description="在手册下进行搜索",priority=21)
 	public void test21() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=手册");
+		webtest.click1("link=内容");
+		webtest.click1("link=手册");
 		webtest.type("id=searchWord", "关于我们");
-		webtest.click("id=submit");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("关于我们"));
 		
 	}
 	@Test(description="在手册下进行搜索",priority=21)
 	public void test22() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=手册");
-		webtest.click("id=submit");
+		webtest.click1("link=内容");
+		webtest.click1("link=手册");
+		webtest.click1("id=submit");
 		assertTrue(webtest.isTextPresent("请输入搜索内容"));
 	}
 	@Test(description="手册下的设置显示第一个option",priority=22)
 	public void test23() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=手册");
-		webtest.click("xpath=/html/body/div/div[1]/div/div/div/div/a[2]");
-		webtest.click("id=index");
-		webtest.click("xpath=//option[@value='1']");
-		webtest.click("id=submit");
+		webtest.click1("link=内容");
+		webtest.click1("link=手册");
+		webtest.click1("xpath=/html/body/div/div[1]/div/div/div/div/a[2]");
+		webtest.click1("id=index");
+		webtest.click1("xpath=//option[@value='1']");
+		webtest.click1("id=submit");
 		ReadProperties u=new ReadProperties();
 		String url=u.getPropertyValue("fronturl");
 		webtest.open(url);
-		webtest.click("link=手册");
+		webtest.click1("link=手册");
 		assertTrue(webtest.isTextPresent("常见问题"));
 	}
 	@Test(description="手册下的设置显示手册列表",priority=22)
 	public void test24() throws IOException {
 		testLogin();
-		webtest.click("link=内容");
-		webtest.click("link=手册");
-		webtest.click("xpath=/html/body/div/div[1]/div/div/div/div/a[2]");
-		webtest.click("id=index");
-		webtest.click("xpath=//option[@value='list']");
-		webtest.click("id=submit");
+		webtest.click1("link=内容");
+		webtest.click1("link=手册");
+		webtest.click1("xpath=/html/body/div/div[1]/div/div/div/div/a[2]");
+		webtest.click1("id=index");
+		webtest.click1("xpath=//option[@value='list']");
+		webtest.click1("id=submit");
 		ReadProperties u=new ReadProperties();
 		String url=u.getPropertyValue("fronturl");
 		webtest.open(url);
-		webtest.click("link=手册");
+		webtest.click1("link=手册");
 		assertTrue(webtest.isTextPresent("用户手册"));
 		assertTrue(webtest.isTextPresent("常见问题"));
 	}
--- chanzhi/src/com/edu/wangxiaodan/shopcity.java
+++ chanzhi/src/com/edu/wangxiaodan/shopcity.java
@@ -17,65 +17,65 @@ public class shopcity extends Admin_Login{
 	@DataProvider
 	public Object[][] leimu() throws IOException {
 		ExcelDataProvider excelDataProvider=new ExcelDataProvider();
-		Object[][] s=excelDataProvider.getTestDataByExcel("F://nnn.xls", "leimu");
+		Object[][] s=excelDataProvider.getTestDataByExcel("F://nnn.xlsx", "leimu");
 		return s;
 	}
 	@Test(description="商城产品下添加类目",priority=1,dataProvider="leimu")
 	public void test1(String name,String name1) throws IOException{
 		this.testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("link=维护类目");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("link=维护类目");
 		webtest.type("id=children[]",name);
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("xpath=//input[@type='submit']");
+		webtest.click1("xpath=//input[@type='submit']");
 		assertTrue(webtest.isTextPresent(name1));
 	}
 	@Test(description="删除四川风味",priority=2)
 	public void test18() throws IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("link=维护类目");
-		webtest.click("xpath=//li[@data-id='4']/a[@class='deleter']");
-		webtest.click("xpath=/html/body/div[2]/div/div/div[2]/button[1]");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("link=维护类目");
+		webtest.click1("xpath=//li[@data-id='4']/a[@class='deleter']");
+		webtest.click1("xpath=/html/body/div[2]/div/div/div[2]/button[1]");
 		assertTrue(webtest.isTextPresent("四川风味"));
 	}
 	@Test(description="添加产品",priority=2)
 	public void test3() throws IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("link=卤煮");
-		webtest.click("link=添加产品");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("link=卤煮");
+		webtest.click1("link=添加产品");
 		webtest.type("xpath=//input[@name='name']", "鸡爪");
 		System.out.println(webtest.isElementPresent("class=ke-edit-iframe"));
 		webtest.enterFrame(1);
-		webtest.click("tag=body");
+		webtest.click1("tag=body");
 		webtest.type("tag=body", "这是一个鸡爪");
 		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("xpath=//input[@type='submit']");
+		webtest.click1("xpath=//input[@type='submit']");
 		assertTrue(webtest.isTextPresent("鸡爪"));
 		
 	}
 	@Test(description="添加名称为空的产品",priority=2)
 	public void test31() throws IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("link=卤煮");
-		webtest.click("link=添加产品");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("link=卤煮");
+		webtest.click1("link=添加产品");
 		System.out.println(webtest.isElementPresent("class=ke-edit-iframe"));
 		webtest.enterFrame(1);
-		webtest.click("tag=body");
+		webtest.click1("tag=body");
 		webtest.type("tag=body", "这是一个鸡爪");
 		webtest.leaveFrame();
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("xpath=//input[@type='submit']");
+		webtest.click1("xpath=//input[@type='submit']");
 		assertTrue(webtest.isTextPresent("不能为空"));
 		
 		
@@ -83,24 +83,24 @@ public class shopcity extends Admin_Login{
 	@Test(description="添加详情为空的产品",priority=2)
 	public void test32() throws IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("link=卤煮");
-		webtest.click("link=添加产品");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("link=卤煮");
+		webtest.click1("link=添加产品");
 		webtest.type("xpath=//input[@name='name']", "鸡爪");
 		System.out.println(webtest.isElementPresent("class=ke-edit-iframe"));
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("xpath=//input[@type='submit']");
+		webtest.click1("xpath=//input[@type='submit']");
 		assertTrue(webtest.isTextPresent("不能为空"));
 		
 	}
 	@Test(description="将鸡爪下架",priority=3)
 	public void test4() throws IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("xpath=");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("xpath=");
 		webtest.open("http://127.0.0.1:8066/chanzhieps/www/index.php/product/c0.html");
 		assertFalse(webtest.isTextPresent("鸡爪"));
 		
@@ -109,9 +109,9 @@ public class shopcity extends Admin_Login{
 	@Test(description="将鸡爪上架",priority=4)
 	public void test5() throws IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/a[4]");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/a[4]");
 		webtest.open("http://127.0.0.1:8066/chanzhieps/www/index.php/product/c0.html");
 		assertTrue(webtest.isTextPresent("鸡爪"));
 		
@@ -120,22 +120,22 @@ public class shopcity extends Admin_Login{
 	@Test(description="搜索",priority=5)
 	public void test6() throws IOException {
 		testLogin();
-		webtest.click("xpath=//a[text()='商城']");
-		webtest.click("xpath=//a[text()='产品']");
+		webtest.click1("xpath=//a[text()='商城']");
+		webtest.click1("xpath=//a[text()='产品']");
 		webtest.type("xpath=//*[@id='searchWord']", "鸡爪");
-		webtest.click("xpath=//*[@id='submit']");
+		webtest.click1("xpath=//*[@id='submit']");
 		assertTrue(webtest.isTextPresent("鸡爪"));
 	}
 	@Test(description="将鸡爪的库存改为600",priority=6)
 	public void test7() throws IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/a[1]");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/a[1]");
 		webtest.typeAndClear("xpath=//*[@id='amount']", "600");
 		webtest.pause(3000);String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("xpath=//*[@id='submit']");
+		webtest.click1("xpath=//*[@id='submit']");
 		assertTrue(webtest.isTextPresent("600"));
 		
 		
@@ -143,13 +143,13 @@ public class shopcity extends Admin_Login{
 	@Test(description="将鸡爪的库存改为0.05",priority=7)
 	public void test8() throws InterruptedException, IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/a[1]");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/a[1]");
 		webtest.typeAndClear("xpath=//*[@id='amount']", "0.05");
 		webtest.pause(3000);String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("xpath=//*[@id='submit']");
+		webtest.click1("xpath=//*[@id='submit']");
 		assertTrue(webtest.isTextPresent("库存应当是数字"));
 		
 		
@@ -157,13 +157,13 @@ public class shopcity extends Admin_Login{
 	@Test(description="将鸡爪的库存改为-600",priority=8)
 	public void test9() throws InterruptedException, IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/a[1]");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/a[1]");
 		webtest.typeAndClear("xpath=//*[@id='amount']", "-600");
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("xpath=//*[@id='submit']");
+		webtest.click1("xpath=//*[@id='submit']");
 		assertTrue(webtest.isTextPresent("库存数量为正整数"));
 		
 		
@@ -171,39 +171,39 @@ public class shopcity extends Admin_Login{
 	@Test(description="将鸡爪删除",priority=9)
 	public void test10() throws IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("link=产品");
-		webtest.click("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/span[2]/a");
-		webtest.click("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/span[2]/ul/li[1]/a");
-		webtest.click("xpath=/html/body/div[2]/div/div/div[2]/button[1]");
+		webtest.click1("link=商城");
+		webtest.click1("link=产品");
+		webtest.click1("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/span[2]/a");
+		webtest.click1("xpath=/html/body/div/div[2]/div/table/tbody/tr[1]/td[8]/span[2]/ul/li[1]/a");
+		webtest.click1("xpath=/html/body/div[2]/div/div/div[2]/button[1]");
 		assertFalse(webtest.isTextPresent("鸡爪"));
 	}
 	@DataProvider
 	public Object[][] name() throws IOException {
 		ExcelDataProvider excelDataProvider=new ExcelDataProvider();
-		Object[][] s=excelDataProvider.getTestDataByExcel("F://nnn.xls", "kuaidi");
+		Object[][] s=excelDataProvider.getTestDataByExcel("F://nnn.xlsx", "kuaidi");
 		return s;
 	}
 	@Test(description="添加快递",priority=10,dataProvider="name")
 	public void test11(String kuaidi,String kuaidi2) throws IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("xpath=/html/body/nav[2]/div[2]/ul[1]/li[3]/a");
-		webtest.click("link=快递");
+		webtest.click1("link=商城");
+		webtest.click1("xpath=/html/body/nav[2]/div[2]/ul[1]/li[3]/a");
+		webtest.click1("link=快递");
 		webtest.typeAndClear("xpath=//*[@id='children[]']", kuaidi);
 		String js_bottom = "var q=document.documentElement.scrollTop=10000";
 		webtest.runJs(js_bottom);
-		webtest.click("xpath=//*[@id='submit']");
+		webtest.click1("xpath=//*[@id='submit']");
 		assertTrue(webtest.isTextPresent(kuaidi2));
 	}
 	@Test(description="删除百世快递",priority=11)
 	public void test12() throws IOException {
 		testLogin();
-		webtest.click("link=商城");
-		webtest.click("xpath=/html/body/nav[2]/div[2]/ul[1]/li[3]/a");
-		webtest.click("link=快递");
-		webtest.click("xpath=//li[@data-id='2']/a");
-		webtest.click("xpath=/html/body/div[2]/div/div/div[2]/button[1]");
+		webtest.click1("link=商城");
+		webtest.click1("xpath=/html/body/nav[2]/div[2]/ul[1]/li[3]/a");
+		webtest.click1("link=快递");
+		webtest.click1("xpath=//li[@data-id='2']/a");
+		webtest.click1("xpath=/html/body/div[2]/div/div/div[2]/button[1]");
 		assertFalse(webtest.isTextPresent("百世"));
 	}
 
