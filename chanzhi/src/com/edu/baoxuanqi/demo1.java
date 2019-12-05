17237335f4ae07b11281e751d2903b2e5ae17af8
wfybxq <33412136+wfybxq@users.noreply.github.com>
2019-12-05 10:28:41 +0800

    Add files via upload

--- chanzhi/src/com/edu/baoxuanqi/demo1.java
+++ chanzhi/src/com/edu/baoxuanqi/demo1.java
@@ -1,9 +1,14 @@
 package com.webtest.demo;
 
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import com.webtest.dataprovider.ExcelDataProvider;
 
 public class demo1 extends Admin_Login{
 	@Test//关键词
@@ -13,6 +18,7 @@ public class demo1 extends Admin_Login{
 		webtest.click("link=关键词");
 		webtest.click("link=链接");
 		webtest.click("xpath=/html/body/div/div/table/tfoot/tr/td/div/a[1]/i");
+		
 	}
 	@Test//关键词点击
 	public void test11() {
@@ -58,7 +64,46 @@ public class demo1 extends Admin_Login{
 	public void test15() {
 		webtest.click("link=推广");
 		webtest.click("link=蝉知");
-		
+	}
+	@Test//关键词页面第二张跳转第一张
+	public void test46() {
+		webtest.click("link=推广");
+		webtest.click("link=关键词");
+		webtest.click("class=icon-play");
+		webtest.click("class=icon icon-play icon-rotate-180");
 		
 	}
+	@Test//关键词页面第二张跳转第一张
+	public void test47() {
+		webtest.click("link=推广");
+		webtest.click("link=关键词");
+		webtest.click("class=icon-step-forward");
+		webtest.click("class=icon-step-backward");
+		
+	}
+	@Test//熊掌号日志
+	public void test48() {
+		webtest.click("link=推广");
+		webtest.click("link=熊掌号");
+		webtest.click("link=日志");
+		webtest.click("link=最近30天");
+		webtest.click("xpath=/html/body/div[1]/div[2]/div/table/tbody/tr[21]/td/div/a[1]");
+		webtest.click("class=icon-step-forward");
+		webtest.click("class=icon-step-backward");
+	}
+	@Test//关键词页面第二张跳转第一张
+	public void test49() {
+		webtest.click("link=推广");
+		webtest.click("link=关键词");
+		webtest.click("id=_recPerPage");
+		webtest.click("link=25");
+	}
+	@Test//熊掌号日志清除内容并输入123
+	public void test50() {
+		webtest.click("link=推广");
+		webtest.click("link=熊掌号");
+		
+		webtest.typeAndClear("id=name", "123");
+	}
+	
 }
